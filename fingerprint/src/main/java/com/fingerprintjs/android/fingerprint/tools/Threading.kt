7dced1855c5c08aa46b62fcec7ec6f17ee6e86cb
Sergey Makarov <sergeytop100@gmail.com>
2022-07-20 14:33:51 +0300

    Workaround for Camera.getNumberOfCameras() hanging forever on Android TV

---
+++ fingerprint/src/main/java/com/fingerprintjs/android/fingerprint/tools/Threading.kt
@@ -0,0 +1,30 @@
+package com.fingerprintjs.android.fingerprint.tools
+
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
+import java.util.concurrent.TimeoutException
+
+private val executor = Executors.newCachedThreadPool()
+
+internal fun <T> await(
+    timeoutMillis: Long,
+    block: () -> T
+): Result<T> {
+    val countDownLatch = CountDownLatch(1)
+    var res: T? = null
+
+    val future = executor.submit {
+        res = block.invoke()
+        countDownLatch.countDown()
+    }
+
+    val completed = countDownLatch.await(timeoutMillis, TimeUnit.MILLISECONDS)
+    if (completed) {
+        @Suppress("UNCHECKED_CAST")
+        return Result.success(res as T)
+    } else {
+        future.cancel(true)
+        return Result.failure(TimeoutException())
+    }
+}
